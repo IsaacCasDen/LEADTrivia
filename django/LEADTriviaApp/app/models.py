cdbfeb46205eb2b0f88ca79a3111933e80fe6d18
IsaacCasDen <isaac.denney@western.edu>
2020-02-04 11:29:59 -0700

    Add app subfolder

---
+++ django/LEADTriviaApp/app/models.py
@@ -0,0 +1,32 @@
+import datetime
+
+from django.db import models
+from django.utils import timezone
+
+class User(models.Model):
+    user_name = models.CharField(max_length=256)
+
+class Team(models.Model):
+    team_name = models.CharField(max_length=256)
+    # team_members = models.ForeignKey(User,on_delete=models.SET_NULL)
+
+class TeamMember(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    team = models.ForeignKey(Team,on_delete=models.CASCADE)
+
+class TriviaGame(models.Model):
+    name = models.CharField(max_length=256)
+
+class TriviaQuestion(models.Model):
+    question = models.CharField(max_length=512)
+    answer = models.CharField(max_length=512)
+
+# class TriviaQuestionMultipleChoice(TriviaQuestion):
+#     pass
+
+# class TriviaQuestionFillInTheBlanks(TriviaQuestion):
+#     pass
+
+class TriviaGameTeams(models.Model):
+    team = models.ForeignKey(Team,on_delete=models.CASCADE)
+    game = models.ForeignKey(TriviaGame,on_delete=models.CASCADE)
