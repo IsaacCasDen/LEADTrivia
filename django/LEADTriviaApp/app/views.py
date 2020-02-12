972f1f6cf2e46a8e98c61ff5190d7030448e9b51
IsaacCasDen <isaac.denney@western.edu>
2020-02-11 17:04:18 -0700

    Added 'team' view

--- django/LEADTriviaApp/app/views.py
+++ django/LEADTriviaApp/app/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import json
+
+from .models import *
 
 # Create your views here.
 
 def index(request):
     return HttpResponse("Where's your use case now???")
+
+
+def team(request):
+    context = {}
+    context['data'] = json.dumps(get_game())
+    return render(request,'team.html',context)
+
+
