9c91545be09f6924ad5400bede908b5dd03b5c71
IsaacCasDen <isaac.denney@western.edu>
2020-02-19 16:23:42 -0700

    Updated team view

--- django/LEADTriviaApp/app/views.py
+++ django/LEADTriviaApp/app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 import json
 
@@ -9,8 +9,29 @@ from .models import *
 def index(request):
     return render(request,'index.html')
 
-
 def team(request):
     context = {}
-    context['data'] = json.dumps(get_game())
+    context['data'] = {}
+    data = get_game()
+
+    id = request.POST.get('teamId','')
+    if id == '':
+        context['data']['team_name'] = "Team {}".format(len(data['Teams'].keys()))
+        context['data']['users'] = ["New User"]
+    else:
+        context['data']['team_name'] = id
+        context['data']['users'] = data['Teams'][id]
     return render(request,'team.html',context)
+    
+
+def lobby(request):
+    context = {}
+    context['data'] = json.dumps(get_game())
+    return render(request, 'lobby.html',context)
+
+def leave_team(request):
+    userId = request.POST['userId']
+    #remove user from team and return to lobby
+
+    return redirect('/app/')
+
