8660e98920d0099bc7380a8fecafba1e377d93d5
IsaacCasDen <isaac.denney@western.edu>
2020-02-20 12:00:01 -0700

    Added route for welcome page

--- django/LEADTriviaApp/app/urls.py
+++ django/LEADTriviaApp/app/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.lobby, name='lobby'),
+    path('',views.index, name='index'),
+    path('lobby/',views.lobby,name='lobby'),
     path('team/',views.team, name='team'),
 ]
