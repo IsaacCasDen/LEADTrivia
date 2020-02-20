ee82a2a6f54a1c3d1133a6d125d13777ffbcd0ed
IsaacCasDen <isaac.denney@western.edu>
2020-02-20 15:00:08 -0700

    Implemented session data

--- django/LEADTriviaApp/app/views.py
+++ django/LEADTriviaApp/app/views.py
@@ -7,8 +7,23 @@ from .models import *
 # Create your views here.
 
 def index(request):
+    game_data = get_game()
+
+    if 'gameId' not in request.session.keys():
+        request.session['gameId'] = game_data['Game'][0]
+
+    if 'userId' not in request.session.keys():
+        request.session['userId'] = None
+
+    if 'errors' not in request.session.keys():
+        request.session['errors'] = []
+
     context = {}
-    context['data'] = json.dumps(get_game())
+    context['gameId'] = request.session['gameId']
+    context['userId'] = request.session['userId']
+    context['errors'] = request.session['errors']
+    context['data'] = json.dumps(game_data)
+
     return render(request,'index.html', context)
 
 def team(request):
@@ -27,15 +42,23 @@ def team(request):
 
 def lobby(request):
     context = {}
-    context['data'] = json.dumps(get_game())
 
     userId = request.POST.get('userId', '')
     gameId = request.POST.get('gameId', '')
-    if userId == '' or gameId == '':
+    if userId == '' and gameId == '':
         return redirect (index)
+    elif gameId == '':
+        context = {}
+        request.session['userId'] = userId
+        request.session['errors'] = json.dumps(['Please enter username'])
+        return redirect(index)
     else:
+        request.session['gameId'] = gameId
         user = new_user(int(gameId), userId)
+        request.session['userId'] = userId
+        context['data'] = json.dumps(get_game())
         if user == None: 
+            request.session['errors'] = json.dumps(['username has already been taken'])
             return redirect (index)
         else:
             return render(request, 'lobby.html',context)
