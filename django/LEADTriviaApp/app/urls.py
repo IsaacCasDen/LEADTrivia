c0369bc8b21d2f6ea76930ee8d0102b9eb6561d0
IsaacCasDen <isaac.denney@western.edu>
2020-03-03 16:40:00 -0700

    Added urls admin_manager, admin_game, create_game, save_game

--- django/LEADTriviaApp/app/urls.py
+++ django/LEADTriviaApp/app/urls.py
@@ -4,6 +4,10 @@ from . import views
 
 urlpatterns = [
     path('',views.index, name='index'),
+    path('admin_manager/', views.admin_manager, name='admin_manager'),
+    path('admin_game/', views.admin_game,name='admin_game'),
+    path('create_game/', views.create_game,name='create_game'),
+    path('save_game/', views.save_game,name='save_game'),
     path('lobby/',views.lobby,name='lobby'),
     path('team/',views.team, name='team'),
     path('mcq/',views.mcq, name='mcquestion'),
@@ -11,4 +15,5 @@ urlpatterns = [
     path('team/update_teamname/',views.update_teamname,name="update_teamname"),
     path('team/update_username/',views.update_username,name="update_username"),
     path('mcq/submit_answer/',views.submitAns,name="submitAns"),
+    path('next_round/',views.next_round,name='next_round')
 ]
