0c9a809bd525a6770ca990babda272544f73a5ca
ParkerPerrine <parker.perrine@western.edu>
2020-03-03 11:27:45 -0700

    route for next question added

--- django/LEADTriviaApp/app/urls.py
+++ django/LEADTriviaApp/app/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('team/update_teamname/',views.update_teamname,name="update_teamname"),
     path('team/update_username/',views.update_username,name="update_username"),
     path('mcq/submit_answer/',views.submitAns,name="submitAns"),
+    path('mcq/next_question/',views.nextQuestion, name="nextQuestion")
 ]
