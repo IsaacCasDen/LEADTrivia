bea5fbfff692ea2216637be15e03102be2cd2c88
ParkerPerrine <parker.perrine@western.edu>
2020-04-15 20:15:41 -0600

    Refactored

--- django/LEADTriviaApp/app/urls.py
+++ django/LEADTriviaApp/app/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    #------------------ADMIN
     path('',views.index, name='index'),
     path('admin_manager/', views.admin_manager, name='admin_manager'),
     path('admin_game/', views.admin_game,name='admin_game'),
@@ -11,7 +12,11 @@ urlpatterns = [
     path('admin_game/', views.admin_game,name='admin_game'),
     path('edit_game/', views.edit_game,name='edit_game'),
     path('create_game/', views.create_game,name='create_game'),
+    path('edit_game/edit_questions/',views.edit_questions,name="edit_questions"),
+    path('current_question_index/', views.current_question_index, name='current_question_index'),
     path('save_game/', views.save_game,name='save_game'),
+
+    #------------------USER
     path('lobby/',views.lobby,name='lobby'),
     path('team/',views.team, name='team'),
     path('show_question/',views.show_question, name='show_question'),
@@ -20,11 +25,8 @@ urlpatterns = [
     path('team/update_username/',views.update_username,name="update_username"),
     path('show_question/submit_answer/',views.submit_answer,name="submit_answer"),
     path('next_round/',views.next_round,name='next_round'),
-    path('show_question/prev_question/',views.prev_question, name="prev_question"),
-    path('show_question/next_question/',views.next_question, name="next_question"),
-    path('current_question_index/', views.current_question_index, name='current_question_index'),
-    path('edit_game/edit_questions/',views.edit_questions,name="edit_questions"),
     path('round_results/', views.round_results, name = "round_results"),
-    path('game_results/', views.game_results, name = "game_results"),
+    path('final_results/', views.final_results, name = "final_results"),
+  
 
 ]
